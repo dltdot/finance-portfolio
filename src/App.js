fc25b5af2f185b2ac04b5b98afc8cf3ce74a18fa
dltdot <ldt1111991@gmail.com>
2025-08-11 12:58:10 +0700

    Add initial project structure and React components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Finance Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
